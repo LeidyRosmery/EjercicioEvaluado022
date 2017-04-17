867d0c13f9de1ba498693e81a10fdf4d281df058
LeidyRosmery <rosm3rita@hotmail.com>
2017-04-17 16:31:48 -0500

    funciona boton cerrar

---
+++ assets/js/app.js
@@ -0,0 +1,16 @@
+var cerrar = document.getElementsByClassName("close");
+var contenedor = document.getElementsByClassName("content-img");
+
+cerrar[0].addEventListener("click", function() {
+    contenedor[0].style.display = "none";
+
+});
+cerrar[1].addEventListener("click", function() {
+    contenedor[1].style.display = "none";
+});
+cerrar[2].addEventListener("click", function() {
+    contenedor[2].style.display = "none";
+});
+cerrar[3].addEventListener("click", function() {
+    contenedor[3].style.display = "none";
+});
