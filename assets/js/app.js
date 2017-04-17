570cea7025d341e5a15c7f92089461fdddd3dead
LeidyRosmery <rosm3rita@hotmail.com>
2017-04-17 17:14:19 -0500

    funcionalidad lista

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,8 @@
 var cerrar = document.getElementsByClassName("close");
 var contenedor = document.getElementsByClassName("content-img");
+var myInfo= document.getElementsByTagName("div");
+console.log(myInfo);
+
 
 cerrar[0].addEventListener("click", function() {
     contenedor[0].style.display = "none";
@@ -13,4 +16,22 @@ cerrar[2].addEventListener("click", function() {
 });
 cerrar[3].addEventListener("click", function() {
     contenedor[3].style.display = "none";
+});
+
+var menu=document.getElementsByClassName("menu");
+var mySeccion=document.getElementsByClassName("galeryImg");
+
+menu[0].addEventListener("click",function(e){
+  e.preventDefault() ;
+  myInfo[1].style.display="none";
+});
+menu[1].addEventListener("click",function(e){
+  e.preventDefault();
+  myInfo[0].style.display="none";
+});
+
+menu[2].addEventListener("click",function(){
+  console.log("ingresa");
+mySeccion[0].style.display="block";
+
 });
