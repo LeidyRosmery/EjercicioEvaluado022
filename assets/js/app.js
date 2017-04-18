f8de23ddb5a5c2abd48871262944074fcee63085
LeidyRosmery <rosm3rita@hotmail.com>
2017-04-17 20:59:13 -0500

    final

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,11 @@
 var cerrar = document.getElementsByClassName("close");
 var contenedor = document.getElementsByClassName("content-img");
 var myInfo = document.getElementsByTagName("div");
-console.log(myInfo);
+//**********************************FUNCION CLOSE IMAGEN *******************
 
 
 cerrar[0].addEventListener("click", function() {
     contenedor[0].style.display = "none";
-
 });
 cerrar[1].addEventListener("click", function() {
     contenedor[1].style.display = "none";
@@ -17,21 +16,20 @@ cerrar[2].addEventListener("click", function() {
 cerrar[3].addEventListener("click", function() {
     contenedor[3].style.display = "none";
 });
-
+// *********************FUNCION DE BOTONES DEL SEGUNDO MENU
 var menu = document.getElementsByClassName("menu");
 var mySeccion = document.getElementsByClassName("galeryImg");
 
+
 menu[0].addEventListener("click", function(e) {
     e.preventDefault();
-    myInfo[1].style.display = "none";
+    document.getElementById("derecha").classList.toggle("ocultar");
 });
 menu[1].addEventListener("click", function(e) {
     e.preventDefault();
-    myInfo[0].style.display = "none";
+    document.getElementById("izquierda").classList.toggle("ocultar");
 });
-
 menu[2].addEventListener("click", function() {
-    console.log("ingresa");
     mySeccion[0].style.display = "block";
-
 });
+//*********************FUNCION DE MOSTRAR Y OCULTAR ********************
