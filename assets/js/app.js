00ef5e25d86eecfd7c6fc2a3ba37cec6dbd4de5c
LeidyRosmery <rosm3rita@hotmail.com>
2017-04-18 11:10:51 -0500

    terminado git add .!

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,35 +1,28 @@
 var cerrar = document.getElementsByClassName("close");
 var contenedor = document.getElementsByClassName("content-img");
 var myInfo = document.getElementsByTagName("div");
-//**********************************FUNCION CLOSE IMAGEN *******************
-
-
-cerrar[0].addEventListener("click", function() {
-    contenedor[0].style.display = "none";
-});
-cerrar[1].addEventListener("click", function() {
-    contenedor[1].style.display = "none";
-});
-cerrar[2].addEventListener("click", function() {
-    contenedor[2].style.display = "none";
-});
-cerrar[3].addEventListener("click", function() {
-    contenedor[3].style.display = "none";
-});
-// *********************FUNCION DE BOTONES DEL SEGUNDO MENU
 var menu = document.getElementsByClassName("menu");
 var mySeccion = document.getElementsByClassName("galeryImg");
+//**********************************FUNCION CLOSE IMAGEN *******************
+function closer(primero, segundo, valor) {
+    primero.addEventListener("click", function() {
+        segundo.style.display = valor;
+    });
+}
+closer(cerrar[0], contenedor[0], "none");
+closer(cerrar[1], contenedor[1], "none");
+closer(cerrar[2], contenedor[2], "none");
+closer(cerrar[3], contenedor[3], "none");
+closer(menu[2], mySeccion[0], "block");
+// *********************FUNCION DE BOTONES DEL SEGUNDO MENU
 
+function myToggle(parm1, parm2) {
+    parm1.addEventListener("click", function(e) {
+        e.preventDefault();
+        document.getElementById(parm2).classList.toggle("ocultar");
+    });
+}
+myToggle(menu[0], "derecha");
+myToggle(menu[1], "izquierda");
 
-menu[0].addEventListener("click", function(e) {
-    e.preventDefault();
-    document.getElementById("derecha").classList.toggle("ocultar");
-});
-menu[1].addEventListener("click", function(e) {
-    e.preventDefault();
-    document.getElementById("izquierda").classList.toggle("ocultar");
-});
-menu[2].addEventListener("click", function() {
-    mySeccion[0].style.display = "block";
-});
 //*********************FUNCION DE MOSTRAR Y OCULTAR ********************
